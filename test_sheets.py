c6f8fe382c6e074dba69e0274c78484144ca7150
Yago <yagodevtech@gmail.com>
2026-05-24 22:57:59 -0300

    feat(app): Updtae credential config

--- test_sheets.py
+++ test_sheets.py
@@ -1,6 +1,11 @@
 import gspread
+import streamlit as st
+from google.oauth2.service_account import Credentials
 
-gc = gspread.service_account(filename="credentials.json")
+credentials = Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+gc = gspread.authorize(credentials)
 
 spreadsheet = gc.open_by_url("https://docs.google.com/spreadsheets/d/1Yu0KC5PH8rvGd520grm-_GMn-5LcuBX5Lx1tmjuO1oo/edit?gid=566375627#gid=566375627")
 
